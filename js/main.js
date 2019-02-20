90f419180aa407fcf0332915e374ee7c9cbb3ccb
Mashiur Rahman <22392318+syedshaon@users.noreply.github.com>
2019-02-20 20:33:45 +0600

    3rd time

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+
+
+
+$(document).ready(function () {
+    new WOW().init();
+        //$("#iphone-screen").animate({ top: "+=700" }, 3);  //This way will create animation which is not needed
+        $(".imgs").delay(2000).css("top", "1px");   
+        $("#iphone-button").delay(1000).animate({ top: "+=5" }, 200);
+        $("#iphone-button").delay(200).animate({ top: "-=4" }, 200);
+        //$("#iphone-button").delay(2600).css("top", "-84px");
+        $("#iphone-screen").delay(1900).show(0);
+       // $("#iphone-screen").delay(1300).animate({ top: "-=700" }, 300);
+       // $("#iphone-off").slideUp("slow");
+        
+});
+
+// This works perfect
+
+//function imgSlideUp() {
+//    document.getElementById("imgs").style.top="1px";
+//}
+//function buttonDown() {
+//    document.getElementById("iphone-button").style.top = "-74px";
+//}
+//function buttonUp() {
+//    document.getElementById("iphone-button").style.top = "-84px";
+//}
+
+//function screenPop() {
+//    document.getElementById("iphone-screen").style.display = "block";
+//}
+
+
+
+//setTimeout(imgSlideUp, 500);
+
+//setTimeout(buttonDown, 1500);
+
+//setTimeout(buttonUp, 1800);
+
+//setTimeout(screenPop, 2100);
+
