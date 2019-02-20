d90ebf98b84d7f89c6ea6aa11ee3df11b03ca7a1
shaon <shaon@gmail.com>
2019-02-20 21:14:54 +0600

    fixed horizontal scrollbar problem

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+var docWidth = document.documentElement.offsetWidth;
 
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function (el) {
+      if (el.offsetWidth > docWidth) {
+          console.log(el);
+      }
+  }
+);
 
 
 
